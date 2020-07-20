ba6872803786c33419701c8190d091f6854ba5f9
=baohoang95 <anhbao0808@gmail.com>
2020-07-20 09:48:08 +0700

    Add Repositories

---
+++ LecyShop.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+//LINQ
+using System.Linq;
+using LecyShop.Data.Infrastructure;
+using LecyShop.Model.Models;
+
+namespace LecyShop.Data.Repositories
+{
+    /// <summary>
+    /// Khai báo các phương thức ko có trong RepositoryBase
+    /// </summary>
+    public interface IProductCategoryRepository
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
+    {
+        public ProductCategoryRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        {
+
+        }
+
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            //x= ProductCategory
+            return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
+        }
+    }
+}
---
+++ LecyShop.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using LecyShop.Data.Infrastructure;
+using LecyShop.Model.Models;
+
+namespace LecyShop.Data.Repositories
+{
+    /// <summary>
+    /// Khai báo các method riêng của Product
+    /// </summary>
+    public interface IProductRepository
+    {
+
+    }
+
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        /// <summary>
+        /// Hàm tạo Product
+        /// </summary>
+        /// <param name="dbFactory"></param>
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
